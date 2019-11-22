12aa72b50482dce2099745d9500398c5a9c26ad8
Vikrant Balyan (vvb) <vvb@cisco.com>
2019-11-22 10:11:15 -0800

    updating path to the new repo
    Signed-off-by: Vikrant Balyan (vvb) <vvb@cisco.com>

--- intersight/data_source_vnic_fc_if.go
+++ intersight/data_source_vnic_fc_if.go
@@ -6,8 +6,8 @@ import (
 	"log"
 	"reflect"
 
+	"github.com/cisco-intersight/terraform-provider-intersight/models"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
-	"github.com/vvb/terraform-provider-intersight/models"
 )
 
 func dataSourceVnicFcIf() *schema.Resource {
