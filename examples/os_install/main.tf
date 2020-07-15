102613957bd9d9cddf191dddbd6bc8cb2fd404c8
code-lucidal58 <aanisha.mishra05@gmail.com>
2020-07-15 23:03:33 +0530

    update examples

--- examples/os_install/main.tf
+++ examples/os_install/main.tf
@@ -1,5 +1,5 @@
 provider "intersight" {
   apikey    = var.api_key
-  secretkeyfile = "/Users/aanimish/Downloads/SecretKeyStagingV2.txt"
-  endpoint = "staging.starshipcloud.com"
+  secretkeyfile = "changeMe"
+  endpoint = "changeMe"
 }
